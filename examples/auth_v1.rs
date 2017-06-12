6946673f2a1e7519a82535a19f83be1250f49b0c
cdumay <cedric.dumay@gmail.com>
2017-06-12 08:45:54 +0200

    devel: example typo

--- examples/auth_v1.rs
+++ examples/auth_v1.rs
@@ -6,6 +6,6 @@ fn main() {
     let client = Client::new("http://127.0.0.1:5000/v1.0", "tester", "testing", None).unwrap();
     match client.authenticate() {
         Ok(data) => println!("Storage URL: {}, Token: {}", data.storage_url, data.auth_token),
-        Err(err) => println!("Error {}", err)
+        Err(err) => println!("Error: {}", err)
     }
 }
--- examples/auth_v2.rs
+++ examples/auth_v2.rs
@@ -6,6 +6,6 @@ fn main() {
     let client = Client::new("http://127.0.0.1:5000/v2.0", "tester", "testing", "test", None).unwrap();
     match client.authenticate() {
         Ok(data) => println!("Token: {} (expired?: {})", data.access.token.id, data.is_token_expired()),
-        Err(err) => println!("Error {}", err)
+        Err(err) => println!("Error: {}", err)
     }
 }
